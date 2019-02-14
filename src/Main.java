69b20ced6889cad55b160daf2242c635f9c7d115
laurentlaurent <lao.laurent@gmail.com>
2019-02-14 01:23:16 -0500

    Tested all Classes

--- src/Main.java
+++ src/Main.java
@@ -86,9 +86,9 @@ public class Main {
 		System.out.println("\tParametized" + new Tram(10.00, 2, 14, 1990, "Foo", "Mick", 100));
 
 		// Metro @TODO
-		//System.out.println("Testing Metro class");
-		//System.out.println("\tDefault: " + new Metro());
-		//System.out.println("\tParametized" + new Metro(9.00, 3, 15, 1989, "Bar", "Arthur", 8, "Montreal"));
+		System.out.println("Testing Metro class");
+		System.out.println("\tDefault: " + new Metro());
+		System.out.println("\tParametized" + new Metro(9.00, 3, 15, 1989, "Bar", "Arthur", 8, "Montreal"));
 	}
 
 	private static void debug_equals() {
@@ -99,38 +99,50 @@ public class Main {
 		Aircraft aircraft1 = new Aircraft(12.34, 3, air.Class.Helicopter, Maintenance.Monthly);
 		Aircraft aircraft2 = new Aircraft(12.34, 3, air.Class.Helicopter, Maintenance.Monthly);
 		Aircraft aircraft3 = new Aircraft(12.30, 3, air.Class.Helicopter, Maintenance.Monthly);
+		Aircraft aircraft4 = null;
 		System.out.println("These should be equal: " + aircraft1.equals(aircraft2));
 		System.out.println("These should be not equal: " + aircraft1.equals(aircraft3));
+		System.out.println("Handles null (should be false): " + aircraft1.equals(aircraft4));
 
 		// Ferry @DONE
 		System.out.println("Testing Ferry class");
 		Ferry ferry1 = new Ferry(12.00, 2, 1991, "Laurent");
 		Ferry ferry2 = new Ferry(12.00, 2, 1991, "Laurent");
 		Ferry ferry3 = new Ferry(12.01, 2, 1991, "Laurent");
+		Ferry ferry4 = null;
 		System.out.println("These should be equal: " + ferry1.equals(ferry2));
 		System.out.println("These should be not equal: " + ferry1.equals(ferry3));
+		System.out.println("Handles null (should be false): " + ferry1.equals(ferry4));
 
 		// City bus @DONE
 		System.out.println("Testing CityBus class");
 		CityBus bus1 = new CityBus(11.00, 1, 13, 1991, "Grand", "Steven");
 		CityBus bus2 = new CityBus(11.00, 1, 13, 1991, "Grand", "Steven");
 		CityBus bus3 = new CityBus(11.00, 1, 13, 1991, "Hehe", "Steven");
+		CityBus bus4 = null;
 		System.out.println("These should be equal: " + bus1.equals(bus2));
 		System.out.println("These should be not equal: " + bus1.equals(bus3));
+		System.out.println("Handles null (should be false): " + bus1.equals(bus4));
 
 		// Tram @TODO
 		System.out.println("Testing Tram class");
 		Tram tram1 = new Tram(10.00, 2, 14, 1990, "Foo", "Mick", 100);
 		Tram tram2 = new Tram(10.00, 2, 14, 1990, "Foo", "Mick", 100);
 		Tram tram3 = new Tram(10.00, 3, 14, 1990, "Foo", "Mick", 100);
+		Tram tram4 = null;
 		System.out.println("These should be equal: " + tram1.equals(tram2));
 		System.out.println("These should be not equal: " + tram1.equals(tram3));
+		System.out.println("Handles null (should be false): " + tram1.equals(tram4));
 
 		// Metro @TODO
-		//System.out.println("Testing Metro class");
-		//Metro metro1 = new Metro(9.00, 3, 15, 1989, "Bar", "Arthur", 8, "Montreal");
-		//System.out.println("These should be equal: " + metro1.equals(metro2));
-		//System.out.println("These should be not equal: " + metro1.equals(metro3));
+		System.out.println("Testing Metro class");
+		Metro metro1 = new Metro(9.00, 3, 15, 1989, "Bar", "Arthur", 8, "Montreal");
+		Metro metro2 = new Metro(9.00, 3, 15, 1989, "Bar", "Arthur", 8, "Montreal");
+		Metro metro3 = new Metro(9.00, 3, 15, 1979, "Bar", "Arthur", 8, "Montreal");
+		Metro metro4 = null;
+		System.out.println("These should be equal: " + metro1.equals(metro2));
+		System.out.println("These should be not equal: " + metro1.equals(metro3));
+		System.out.println("Handles null (should be false): " + metro1.equals(metro4));
 	}
 
 	// End of code testers
